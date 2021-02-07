842c527f8dacedf7c3b8d144774e574ce3de8d54
Kwin <kwin@promote.cache-dns.local>
2021-02-08 05:34:10 +0800

    添加demo目录

---
+++ demo/src/com/kkk/test/Server.java
@@ -0,0 +1,13 @@
+package com.kkk.test;
+
+public class Server {
+
+    /**
+     * main 方法
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+}
